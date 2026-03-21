2a723fdea9afe4df6f43022a9518734be11985ac
Andrew Cummings <acummings@egnyte.com>
2026-03-20 22:31:32 -0700

    better colors?

--- hex.js
+++ hex.js
@@ -125,7 +125,7 @@ function renderHexGrid(canvas, ctx, camX, camY, moves, highlightIndex, winMoves)
 
 		let fill = move.p1 ? "#3a2a2a" : "#2a2a3a";
 		if (highlightIndex < 0 && winMoves && winMoves.includes(i)) {
-			fill = move.p1 ? "#4d3838" : "#3c3c51";
+			fill = move.p1 ? "#4d3838" : "#3a3a4f";
 		}
 		let stroke = move.p1 ? "#6a4a4a" : "#4a4a6a";
 		if (highlightIndex >= 0 && i === highlightIndex) {
